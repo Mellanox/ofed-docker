0ad2c7b5fcc0342eac336d1253f0fe60e417eeee
Adrian Chiris <adrianc@mellanox.com>
2020-07-05 13:26:46 +0300

    Refactor entrypoints
    Refactor both ofed and gpu-direct entrypoints
    
    - Fail container in case of error instead of simply going to
      `sleep inf`
    - Partition code into functions for better file structuring
    - In ofed driver container trigger dkms autoinstall in case driver
      loading failed. This, in case of a new kernel will trigger a rebuild
      of the drivers.

--- gpu-direct/ubuntu/entrypoint.sh
+++ gpu-direct/ubuntu/entrypoint.sh
@@ -2,23 +2,51 @@
 MOFED=/run/mellanox/drivers
 NVIDIA=/run/nvidia/drivers
 KERNEL_VERSION=$(uname -r)
-# Install linux headers
-apt-get -yq install linux-headers-${KERNEL_VERSION}
-# Patch filesystem with components from both Mellanox and Nvidia Drivers
-ln -sf ${MOFED}/usr/src/ofa_kernel /usr/src/ofa_kernel
-ln -sf ${NVIDIA}/usr/src/nvidia-* /usr/src/.
-touch /lib/modules/${KERNEL_VERSION}/modules.order
-touch /lib/modules/${KERNEL_VERSION}/modules.builtin
-mkdir -p /lib/modules/${KERNEL_VERSION}/updates/dkms
-ln -sf ${MOFED}/lib/modules/${KERNEL_VERSION}/updates/dkms/* /lib/modules/${KERNEL_VERSION}/updates/dkms/
-ln -sf ${NVIDIA}/lib/modules/${KERNEL_VERSION}/updates/dkms/* /lib/modules/${KERNEL_VERSION}/updates/dkms/
-ln -sf ${MOFED}/var/lib/dkms/mlnx-ofed-kernel /var/lib/dkms/mlnx-ofed-kernel
-ln -sf ${NVIDIA}/var/lib/dkms/nvidia /var/lib/dkms/nvidia
-mkdir -p /etc/infiniband
-cp /root/nv_peer_memory/nv_peer_mem.conf /etc/infiniband/
-# Build NV PEER MEMORY module
-cd /root/nv_peer_memory
-make clean && make && make install
-./nv_peer_mem restart
-./nv_peer_mem status
+
+function exit_on_error() {
+    $@
+    if [[ $? -ne 0 ]]; then
+        echo "Error occured while executing: $1"
+        exit 1
+    fi
+}
+
+function install_prereq_runtime() {
+    # Install linux headers
+    apt-get -yq install linux-headers-${KERNEL_VERSION}
+}
+
+function prepare_build_env() {
+    # Patch filesystem with components from both Mellanox and Nvidia Drivers
+    ln -sf ${MOFED}/usr/src/ofa_kernel /usr/src/ofa_kernel
+    ln -sf ${NVIDIA}/usr/src/nvidia-* /usr/src/.
+    touch /lib/modules/${KERNEL_VERSION}/modules.order
+    touch /lib/modules/${KERNEL_VERSION}/modules.builtin
+    mkdir -p /lib/modules/${KERNEL_VERSION}/updates/dkms
+    ln -sf ${MOFED}/lib/modules/${KERNEL_VERSION}/updates/dkms/* /lib/modules/${KERNEL_VERSION}/updates/dkms/
+    ln -sf ${NVIDIA}/lib/modules/${KERNEL_VERSION}/updates/dkms/* /lib/modules/${KERNEL_VERSION}/updates/dkms/
+    ln -sf ${MOFED}/var/lib/dkms/mlnx-ofed-kernel /var/lib/dkms/mlnx-ofed-kernel
+    ln -sf ${NVIDIA}/var/lib/dkms/nvidia /var/lib/dkms/nvidia
+    mkdir -p /etc/infiniband
+    cp /root/nv_peer_memory/nv_peer_mem.conf /etc/infiniband/
+}
+
+function build_modules() {
+    # Build NV PEER MEMORY module
+    cd /root/nv_peer_memory
+    make clean && make && make install
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+    ./nv_peer_mem stop
+    ./nv_peer_mem start
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+    ./nv_peer_mem status
+}
+
+exit_on_error install_prereq_runtime
+exit_on_error prepare_build_env
+exit_on_error build_modules
 sleep infinity
--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -x
-mount_rootfs() {
+
+function exit_on_error() {
+    $@
+    if [[ $? -ne 0 ]]; then
+        echo "Error occured while executing: $1"
+        exit 1
+    fi
+}
+
+function mount_rootfs() {
     echo "Mounting OFED driver container rootfs..."
     mount --make-runbindable /sys
     mount --make-private /sys
@@ -7,15 +16,33 @@ mount_rootfs() {
     mount --rbind / /run/mellanox/drivers
 }
 
-unmount_rootfs() {
+function unmount_rootfs() {
     echo "Unmounting Mellanox OFED driver rootfs..."
     if findmnt -r -o TARGET | grep "/run/mellanox/drivers" > /dev/null; then
       umount -l -R /run/mellanox/drivers
     fi
 }
 
-/etc/init.d/openibd restart
-ofed_info -s
+function rebuild_driver() {
+    # Rebuild driver in case installed driver kernel version differs from running kernel
+    echo "Rebuilding driver"
+    apt-get -yq update
+    apt-get -yq install linux-headers-$(uname -r)
+    dkms autoinstall
+}
+function start_driver() {
+    /etc/init.d/openibd stop
+    /etc/init.d/openibd start
+    if [[ $? -ne 0 ]]; then
+        echo "Error occured while starting driver"
+        rebuild_driver
+        /etc/init.d/openibd start
+        return $?
+    fi
+    ofed_info -s
+}
+
+exit_on_error start_driver
 mount_rootfs
 trap "echo 'Caught signal'; exit 1" HUP INT QUIT PIPE TERM
 trap "unmount_rootfs" EXIT
